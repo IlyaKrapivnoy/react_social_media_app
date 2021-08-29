8247d80c8bcc7f3a093a7b13edb27dc7f66d81f4
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-29 20:54:26 +0300

    add share card style

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,5 +1,5 @@
 import './share.css';
-import { PermMedia } from '@material-ui/icons';
+import { PermMedia, Label, Room, EmojiEmotions } from '@material-ui/icons';
 
 const Share = () => {
     return (
@@ -8,7 +8,7 @@ const Share = () => {
                 <div className='shareTop'>
                     <img
                         className='shareProfileImg'
-                        // src='/assets/person/1.jpeg'
+                        src='/assets/person/1.jpeg'
                         alt='user'
                     />
                     <input
@@ -20,12 +20,37 @@ const Share = () => {
                 <div className='shareBottom'>
                     <div className='shareOptions'>
                         <div className='shareOption'>
-                            <PermMedia className='shareIcon' />
+                            <PermMedia
+                                htmlColor='tomato'
+                                className='shareIcon'
+                            />
                             <span className='shareOptionText'>
                                 Photo or Video
                             </span>
                         </div>
                     </div>
+                    <div className='shareOptions'>
+                        <div className='shareOption'>
+                            <Label htmlColor='blue' className='shareIcon' />
+                            <span className='shareOptionText'>Tag</span>
+                        </div>
+                    </div>
+                    <div className='shareOptions'>
+                        <div className='shareOption'>
+                            <Room htmlColor='green' className='shareIcon' />
+                            <span className='shareOptionText'>Location</span>
+                        </div>
+                    </div>
+                    <div className='shareOptions'>
+                        <div className='shareOption'>
+                            <EmojiEmotions
+                                htmlColor='goldenrod'
+                                className='shareIcon'
+                            />
+                            <span className='shareOptionText'>Feelings</span>
+                        </div>
+                    </div>
+                    <button className='shareButton'>Share</button>
                 </div>
             </div>
         </div>
