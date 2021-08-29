b48876380560652961aaaffa77d432bafaf55ce3
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-29 22:12:00 +0300

    add online friends section

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -16,6 +16,126 @@ const Rightbar = () => {
                     </span>
                 </div>
                 <img className='rightbarAd' src='/assets/ad.png' alt='ad' />
+                <h4 className='rightbarTitle'>Online Friends</h4>
+                <ul className='rightbarFriendList'>
+                    <li className='rightbarFriend'>
+                        <div className='rightbarProfileImgContainer'>
+                            <img
+                                className='rightbarProfileImg'
+                                src='assets/person/3.jpeg'
+                                alt='ad'
+                            />
+                            <span className='rightbarOnline'></span>
+                        </div>
+                        <span className='rightbarUsername'>
+                            Milhouse Van Houten
+                        </span>
+                    </li>
+                    <li className='rightbarFriend'>
+                        <div className='rightbarProfileImgContainer'>
+                            <img
+                                className='rightbarProfileImg'
+                                src='assets/person/4.jpeg'
+                                alt='ad'
+                            />
+                            <span className='rightbarOnline'></span>
+                        </div>
+                        <span className='rightbarUsername'>
+                            Milhouse Van Houten
+                        </span>
+                    </li>
+                    <li className='rightbarFriend'>
+                        <div className='rightbarProfileImgContainer'>
+                            <img
+                                className='rightbarProfileImg'
+                                src='assets/person/5.jpeg'
+                                alt='ad'
+                            />
+                            <span className='rightbarOnline'></span>
+                        </div>
+                        <span className='rightbarUsername'>
+                            Milhouse Van Houten
+                        </span>
+                    </li>
+                    <li className='rightbarFriend'>
+                        <div className='rightbarProfileImgContainer'>
+                            <img
+                                className='rightbarProfileImg'
+                                src='assets/person/6.jpeg'
+                                alt='ad'
+                            />
+                            <span className='rightbarOnline'></span>
+                        </div>
+                        <span className='rightbarUsername'>
+                            Milhouse Van Houten
+                        </span>
+                    </li>
+                    <li className='rightbarFriend'>
+                        <div className='rightbarProfileImgContainer'>
+                            <img
+                                className='rightbarProfileImg'
+                                src='assets/person/7.jpeg'
+                                alt='ad'
+                            />
+                            <span className='rightbarOnline'></span>
+                        </div>
+                        <span className='rightbarUsername'>
+                            Milhouse Van Houten
+                        </span>
+                    </li>
+                    <li className='rightbarFriend'>
+                        <div className='rightbarProfileImgContainer'>
+                            <img
+                                className='rightbarProfileImg'
+                                src='assets/person/8.jpeg'
+                                alt='ad'
+                            />
+                            <span className='rightbarOnline'></span>
+                        </div>
+                        <span className='rightbarUsername'>
+                            Milhouse Van Houten
+                        </span>
+                    </li>
+                    <li className='rightbarFriend'>
+                        <div className='rightbarProfileImgContainer'>
+                            <img
+                                className='rightbarProfileImg'
+                                src='assets/person/9.jpeg'
+                                alt='ad'
+                            />
+                            <span className='rightbarOnline'></span>
+                        </div>
+                        <span className='rightbarUsername'>
+                            Milhouse Van Houten
+                        </span>
+                    </li>
+                    <li className='rightbarFriend'>
+                        <div className='rightbarProfileImgContainer'>
+                            <img
+                                className='rightbarProfileImg'
+                                src='assets/person/10.jpeg'
+                                alt='ad'
+                            />
+                            <span className='rightbarOnline'></span>
+                        </div>
+                        <span className='rightbarUsername'>
+                            Milhouse Van Houten
+                        </span>
+                    </li>
+                    <li className='rightbarFriend'>
+                        <div className='rightbarProfileImgContainer'>
+                            <img
+                                className='rightbarProfileImg'
+                                src='assets/person/1.jpeg'
+                                alt='ad'
+                            />
+                            <span className='rightbarOnline'></span>
+                        </div>
+                        <span className='rightbarUsername'>
+                            Milhouse Van Houten
+                        </span>
+                    </li>
+                </ul>
             </div>
         </div>
     );
