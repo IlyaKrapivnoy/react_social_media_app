a0960190cf5f4f9a37c15fc804071cb0917d0cb3
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-30 22:37:28 +0300

    add profile infor with cover section

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -12,7 +12,26 @@ const Profile = () => {
             <div className='profile'>
                 <Sidebar />
                 <div className='profileRight'>
-                    <div className='profileTop'>TESTTEST</div>
+                    <div className='profileTop'>
+                        <div className='profileCover'>
+                            <img
+                                alt='cover'
+                                src='assets/post/3.jpeg'
+                                className='profileCoverImg'
+                            />
+                            <img
+                                alt='user'
+                                src='assets/person/7.jpeg'
+                                className='profileUserImg'
+                            />
+                        </div>
+                        <div className='profileInfo'>
+                            <h4 className='profileInfoName'>Bart Simpson</h4>
+                            <span className='profileInfoDescr'>
+                                Hello my friends!
+                            </span>
+                        </div>
+                    </div>
                     <div className='profileBottom'>
                         <Feed />
                         <Rightbar />
