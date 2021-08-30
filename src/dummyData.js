5cf7ecd801f5f8067c1798626bdd55a37f7cf54a
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-30 20:10:51 +0300

    add user data

---
+++ src/dummyData.js
@@ -0,0 +1,52 @@
+export const Users = [
+    {
+        id: 1,
+        profilePicture: 'assets/person/1.jpeg',
+        userName: 'Miss Hoover',
+    },
+    {
+        id: 2,
+        profilePicture: 'assets/person/2.jpeg',
+        userName: 'Groundskeeper Willie',
+    },
+    {
+        id: 3,
+        profilePicture: 'assets/person/3.jpeg',
+        userName: 'Grampa Simpson',
+    },
+    {
+        id: 4,
+        profilePicture: 'assets/person/4.jpeg',
+        userName: 'Chief Wiggum',
+    },
+    {
+        id: 5,
+        profilePicture: 'assets/person/5.jpeg',
+        userName: 'Dr. Hibbert',
+    },
+    {
+        id: 6,
+        profilePicture: 'assets/person/6.jpeg',
+        userName: 'Lenny Leonard',
+    },
+    {
+        id: 7,
+        profilePicture: 'assets/person/7.jpeg',
+        userName: 'Milhouse Van Houten',
+    },
+    {
+        id: 8,
+        profilePicture: 'assets/person/8.jpeg',
+        userName: 'Allison Taylor',
+    },
+    {
+        id: 9,
+        profilePicture: 'assets/person/9.jpeg',
+        userName: 'Selma Bouvier',
+    },
+    {
+        id: 10,
+        profilePicture: 'assets/person/10.jpeg',
+        userName: 'Edna Krabappel',
+    },
+];
